49b00204ff9f0ee62cd7fb62028391b705274769
Alan Zhang <Alan Zhang>
2017-03-18 17:18:17 -0400

    Fixed typing

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -7,4 +7,4 @@ class SignupForm(forms.Form):
 	#CharField and EmailField are only two of them
 	#go through the official docs for more field details
 	name = forms.CharField(label='Enter your name', max_length=100)
-	email = forms.EmailField(label='Enter your favorite Pokemon', max_length=100)
+	email = forms.EmailField(label='Enter your email', max_length=100)
